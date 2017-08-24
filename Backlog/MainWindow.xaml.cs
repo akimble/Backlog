69403710e98042c86986038404c11bc573e6de88
akimble <kimble00550117@gmail.com>
2017-08-24 13:59:39 -0400

    MainWindow inherits from NavigationWindow

--- Backlog/MainWindow.xaml.cs
+++ Backlog/MainWindow.xaml.cs
@@ -18,12 +18,11 @@ namespace Backlog
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : NavigationWindow
     {
         public MainWindow()
         {
             InitializeComponent();
-            MainWindowFrame.NavigationService.Navigate(new BacklogsPage());
         }
     }
 }
